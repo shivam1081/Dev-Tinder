351016413c38f98b0c5f8cfbc12fac9d92c0d5e1
Shivam Dubey <shivamdubey1081@gmail.com>
2025-04-26 21:22:21 +0530

    Exploring the GET, POST, DELETE Calls.

--- devTinder-backend/src/app.js
+++ devTinder-backend/src/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res, next) => {
-  res.send("Hello from the server");
+// This is the top level one and could be called in all scenarios
+app.use("/user", (req, res) => {
+  res.send("Shivam Dubey");
 });
+
+// It is exclusively for the get call
+app.get("/user", (req, res, next) => {
+  res.send("Making a get call");
+});
+
+// It is for the Post Call
+app.post("/user", (req, res) => {
+  res.send("Making a post call");
+});
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
- 
